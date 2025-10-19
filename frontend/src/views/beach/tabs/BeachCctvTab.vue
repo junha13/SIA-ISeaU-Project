a2a83e941c5af532ad7121328dadb7810741f7aa
minmin721 <lghpds000@naver.com>
2025-10-19 20:49:07 +0900

    cctv 하다 맘

--- frontend/src/views/beach/tabs/BeachCctvTab.vue
+++ frontend/src/views/beach/tabs/BeachCctvTab.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="p-3">
-    <h6 class="fw-bold mb-2">CCTV</h6>
-    <small class="text-muted">25.10.16 3시 기준</small>
-    <div class="rounded bg-light border d-flex justify-content-center align-items-center" style="height:200px;">
-      CCTV View (해수욕장: {{ detailData?.name }})
-    </div>
+  <div class="cctv-player">
+    <video ref="video" controls autoplay muted playsinline width="100%" height="360"></video>
   </div>
 </template>
 
-<script setup>
-import { defineProps } from 'vue';
+<script>
+import Hls from 'hls.js';
 
-defineProps({
-  detailData: Object,
-});
+export default {
+  name: 'BeachCctvTab',
+  props: {
+    src: { type: String, required: true } // m3u8 전체 URL
+  },
+  mounted() {
+    const video = this.$refs.video;
+    const url = this.src;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => video.play().catch(()=>{}));
+      this._hls = hls;
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = url;
+    } else {
+      console.error('브라우저가 HLS를 지원하지 않습니다.');
+    }
+  },
+  beforeDestroy() {
+    if (this._hls) this._hls.destroy();
+  }
+}
 </script>
+
+<style scoped>
+.cctv-player { display:flex; justify-content:center; }
+video { background:#000; }
+</style>
