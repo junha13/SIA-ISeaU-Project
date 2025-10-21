ecdb997b2bcb95d77f62d2e8c5e14c0dcb613eb5
rhdpdnjs116-source <rhdpdnjs116@gmail.com>
2025-10-21 17:15:33 +0900

    backend:beach부분, 프론트beach.id->beach.beachnumber로수정/yewon

--- backend/springboot/src/main/java/lx/iseau/feature/beach/BeachDAO.java
+++ backend/springboot/src/main/java/lx/iseau/feature/beach/BeachDAO.java
@@ -3,9 +3,10 @@ package lx.iseau.feature.beach;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List; // java.util.List 임포트 필요
+import java.util.List;
 
 @Mapper
 public interface BeachDAO {
+	List<BeachVO> findBeaches(BeachListRequest request);
 
 }
--- backend/springboot/src/main/java/lx/iseau/feature/beach/BeachService.java
+++ backend/springboot/src/main/java/lx/iseau/feature/beach/BeachService.java
@@ -1,6 +1,9 @@
+
 package lx.iseau.feature.beach;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,21 +11,43 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class BeachService {
+public class BeachService  { 
+
+    @Autowired
+    BeachDAO dao;
+    @Transactional(readOnly = true)
+    
+    public List<BeachDTO> getBeachList(BeachListRequest request) {
+        List<BeachVO> voList = dao.findBeaches(request);
+        List<BeachDTO> dtoList = new ArrayList<>();      
+        for (BeachVO vo : voList) {
+            BeachDTO dto = new BeachDTO(
+                vo.getBeachNumber(),
+                vo.getBeachName(),
+                vo.getBeachImage(),
+                vo.getBeachInformation(),
+                vo.getRating(),
+                vo.isApprovedByMinistry(),
+                vo.getAddress(),
+                vo.getLatitude(),
+                vo.getLongitude(), 
+                vo.getMobile(),
+                vo.getOpenDate(),
+                vo.getCloseDate()
+            );
+            dtoList.add(dto);
+        }
+       
+        Map<String, Object> map = new HashMap<String, Object>();
+        
+        
+        map.put("result", dtoList);
+        
+      
+        return dtoList;
+    }
+
 	
-	@Autowired
-	BeachDAO dao;
-	
-	/*
-	 * 설명
-	 */
-	@Transactional
-	public Map<String, Object> a(int num) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		
-		map.put("result", num);
-		
-		return map;
-	}
-	
+    
+  
 }
