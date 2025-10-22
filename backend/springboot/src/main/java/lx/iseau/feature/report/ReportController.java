3ff7569ef758f013a355fad5f45fcc65c712a6d1
minmin721 <lghpds000@naver.com>
2025-10-22 19:06:21 +0900

    sos 여전히 진행중..

--- backend/springboot/src/main/java/lx/iseau/feature/report/ReportController.java
+++ backend/springboot/src/main/java/lx/iseau/feature/report/ReportController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +27,17 @@ public class ReportController {
 				.header("api", "Report/first-aid")
 				.body(Map.of("data", result));
 	}
+	
+	/*
+	 * ===== 상황목록 조회 =====
+	 */ 
+	@GetMapping("/first-aid/cases")
+	public ResponseEntity<?> listFirstAidCases() {
+		Map<String, Object> result = service.listFirstAidCases();
+		return ResponseEntity
+				.ok()
+				.header("api", "Report/first-aid/cases")
+				.body(Map.of("data", result));
+	}
+	
 }
