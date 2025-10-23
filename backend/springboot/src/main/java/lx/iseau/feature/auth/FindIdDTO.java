aa906ab3b3fa0afc4aa89746e65a370d222c2d6a
jiseoheo <jiseo.heo@gmail.com>
2025-10-23 22:50:09 +0900

    아이디찾기 backend_확정X

---
+++ backend/springboot/src/main/java/lx/iseau/feature/auth/FindIdDTO.java
@@ -0,0 +1,23 @@
+package lx.iseau.feature.auth;
+
+public class FindIdDTO {
+    private String userName;
+    private String mobile;
+
+    // Getters and Setters
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
