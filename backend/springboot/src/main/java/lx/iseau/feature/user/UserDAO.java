e7c4766d010bbb4ebf34281b034efe455892879b
jiseoheo <jiseo.heo@gmail.com>
2025-10-23 11:34:54 +0900

    로그인/회원가입 backend 1차_확인X

--- backend/springboot/src/main/java/lx/iseau/feature/user/UserDAO.java
+++ backend/springboot/src/main/java/lx/iseau/feature/user/UserDAO.java
@@ -3,9 +3,24 @@ package lx.iseau.feature.user;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List; // java.util.List 임포트 필요
 
 @Mapper
 public interface UserDAO {
+	/*
+	 * 로그인
+	 * id와 비밀번호가 모두 DB에 있다면 결과의 userId를 뱉는다.
+	 */
+	int login(@Param("id") String id, @Param("pW") String pW);
+	
+	/*
+	 * 회원가입
+	 */
+    int register(RegisterDTO user);
 
+    
+    /*
+     * 중복확인
+     */
+    int checkDuplicateId(@Param("id") String id);
+    
 }
