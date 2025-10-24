4c339c86ce8fb899b8d4e2bd3ad458db97d22044
junha13 <tnznf1113@gmail.com>
2025-10-24 17:53:36 +0900

    echart 적용 완료 / junha

--- frontend/src/views/beach/tabs/BeachDangerTab.vue
+++ frontend/src/views/beach/tabs/BeachDangerTab.vue
@@ -14,8 +14,10 @@
 
     <div v-if="active === 'rip'">
       <h6 class="fw-bold mb-3">이안류 안전등급: <span :style="{ color: dangerData?.rip === '주의' ? '#FFB354' : '#8482FF' }">{{ dangerData?.rip || '정보 없음' }}</span></h6>
-      <div class="border rounded p-3 text-center small">
-        <p>관심 ~ 위험 등급을 그래프로 시각화</p>
+      <div class="border rounded text-center small h-500px">
+        <span>
+          <v-chart :option="option" autoresize class="chart-fill h-50" />
+        </span>
       </div>
     </div>
     <div v-else-if="active === 'jelly'">
@@ -56,7 +58,37 @@ const tabStyle = (tab) => ({
 })
 
 if (active === "rip") {
-  
+
 }
 const dangerData = computed(() => props.detailData?.danger);
+
+
+// 차트 옵션만들기
+let option = {
+  // 차트 제목
+  title: {
+    text: 'ddd',
+  },
+  // 범례명
+  legend: {
+    data: ['개수'],
+    top: 20,
+  },
+  // x축 라벨
+  xAxis: {
+    data: ['00', '01', '02', '03', '04', '05', '06', '07',
+           '08', '09', '10', '11', '12', '13', '14', '15',
+           '16', '17', '18', '19', '20', '21', '22', '23',
+    ],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '개수',
+      type: 'bar', // 막대 그래프
+      data: [5, 3, 4],
+    },
+  ],
+}
+
 </script>
