b76c6985473a259580ac261e5d16965326c9687f
Yeongmin8383@gmail.com <Yeongmin8383@gmail.com>
2025-10-29 20:27:53 +0900

    CCTV 추가 / yeongmin

--- frontend/src/constants/cctvMap.js
+++ frontend/src/constants/cctvMap.js
@@ -1,12 +1,23 @@
 export const CCTV_BY_BEACH_NAME = {
   '속초해수욕장': 'https://www.youtube.com/live/mUyJ30QxPMg?si=jj4eXGWXqhcVDWqT',
   '해운대해수욕장': 'https://www.youtube.com/live/MWYxOlqF7E8?si=i5wV4CdEx3Z_yvKl',
-  '이호테우해수욕장':'http://ubin.onpr.co.kr/bbs/board.php?bo_table=cctv&wr_id=53',
+  '이호테우해수욕장': 'http://ubin.onpr.co.kr/bbs/board.php?bo_table=cctv&wr_id=53',
   '강문해수욕장': 'https://www.youtube.com/live/Al-_GZNWmKQ?si=XMWo8eMWwhXeD3rb',
-  '광안리해수욕장':'https://www.youtube.com/live/E9I4A9aXJUY?si=xXeUQosxIc9mbbFk',
-  '함덕해수욕장':'http://ubin.onpr.co.kr/bbs/board.php?bo_table=cctv&wr_id=52',
+  '광안리해수욕장': 'https://www.youtube.com/live/E9I4A9aXJUY?si=xXeUQosxIc9mbbFk',
+  '함덕해수욕장': 'http://ubin.onpr.co.kr/bbs/board.php?bo_table=cctv&wr_id=52',
+  '협재해수욕장': 'http://211.114.96.121:1935/jejusi6/11-17.stream/playlist.m3u8',
+  '중문해수욕장': 'http://59.8.86.94:8080/media/api/v1/hls/vurix/192871/100010/0/1/1.m3u8',
+  '구룡포해수욕장': 'https://safecctv.pohang.go.kr/100036',
+  '송정해수욕장': 'https://www.youtube.com/embed/mAXNPDH6UW4?si=KDxU5c_Cs9Y4XDaY',
+  '신지명사십리해수욕장': 'https://live.knps.or.kr/cctv/hls/jodo.m3u8',
+  '방아머리해변': 'https://coast.mof.go.kr/proxy.jsp?http://10.176.62.134:9001/tilemapApi.do?url=http://220.95.232.18/camera/72_0.jpg?1761637017960%27'
 };
 export const CCTV_BY_BEACH_NUMBER = {
   // 예: '202': 'https://www.youtube.com/live/mUyJ30QxPMg?si=jj4eXGWXqhcVDWqT', // 속초
   //     '101': 'https://www.youtube.com/live/MWYxOlqF7E8?si=i5wV4CdEx3Z_yvKl', // 해운대
 };
+
+
+
+
+
